8b47ee2350509f55c1138cbe3747aa7891780303
yermxx <66yrlee99@gmail.com>
2024-11-30 13:21:04 +0900

    ✨ (React) Update useTimer

--- React/rbvite/src/hooks/timer-hooks.ts
+++ React/rbvite/src/hooks/timer-hooks.ts
@@ -1,19 +1,42 @@
 import { useEffect } from 'react';
 
-export const useTimeout = (cb: () => void, delay: number) => {
+// type safety !!
+export const useTimeout = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+  cb: T,
+  delay: number,
+  ...args: Parameters<T>
+) => {
   useEffect(() => {
-    const intl = setTimeout(cb, delay);
-    return () => clearInterval(intl);
-  }, [cb, delay]);
+    const timer = setTimeout(cb, delay, ...args);
+    return () => clearTimeout(timer);
+  }, [cb, delay, args]);
 };
 
-export const useInterval = <T extends unknown[]>(
-  cb: (...args: T) => void,
+export const useInterval = <T extends (...args: unknown[]) => void>(
+  cb: T,
   delay: number,
-  ...args: T
+  ...args: Parameters<T>
 ) => {
   useEffect(() => {
     const intl = setInterval(cb, delay, ...args);
     return () => clearInterval(intl);
   }, [args, cb, delay]);
 };
+
+// export const useTimeout = (cb: () => void, delay: number) => {
+//   useEffect(() => {
+//     const intl = setTimeout(cb, delay);
+//     return () => clearInterval(intl);
+//   }, [cb, delay]);
+// };
+
+// export const useInterval = <T extends unknown[]>(
+//   cb: (...args: T) => void,
+//   delay: number,
+//   ...args: T
+// ) => {
+//   useEffect(() => {
+//     const intl = setInterval(cb, delay, ...args);
+//     return () => clearInterval(intl);
+//   }, [args, cb, delay]);
+// };
