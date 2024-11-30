1109064f692a54280af472a28fe438b576749934
yermxx <66yrlee99@gmail.com>
2024-11-30 13:03:37 +0900

    ✨ (React) Update useToggle

--- React/rbvite/src/hooks/useToggle.ts
+++ React/rbvite/src/hooks/useToggle.ts
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 
-export const useToggle = () => {
-  const [isShow, setShow] = useState(false);
-  const toggle = () => setShow((pre) => !pre);
+// export const useToggle = () => {
+//   const [isShow, setShow] = useState(false);
+//   const toggle = () => setShow((pre) => !pre);
 
-  return [isShow, toggle] as const; // tuple 형태로 return
+//   return [isShow, toggle] as const; // tuple 형태로 return
+// };
+
+// 기본값 설정 ver
+export const useToggle = (defVal: boolean = false) => {
+  const [state, setState] = useState(defVal);
+  const toggle = (forceState?: unknown) =>
+    setState((pre) => (typeof forceState === 'boolean' ? forceState : !pre));
+
+  return [state, toggle] as const;
 };
