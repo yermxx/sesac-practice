ef9716759c55403aa087b2615c182072e55207f0
yermxx <66yrlee99@gmail.com>
2024-12-12 16:47:21 +0900

    ✨ (Nextjs) add useRouter, usePathname, useSearchParams

--- next/app14/app/(routes)/about/me/page.tsx
+++ next/app14/app/(routes)/about/me/page.tsx
@@ -1,3 +1,34 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 export default function Me() {
-  return <>Me Page</>;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const urlSearchParams = new URLSearchParams(searchParams.toString());
+
+  const goBack = () => router.back();
+
+  const goHello = () => router.push('/hello', { scroll: false });
+
+  // 보통은 custom-hook을 만들어서 사용
+  const changeSearchParams = (x: string) => {
+    urlSearchParams.set('xxx', x);
+    // console.log('urlSearchParams', urlSearchParams.toString());
+    router.push(`${pathname}?${urlSearchParams.toString()}`); // router.push를 해야지만 url창에 반영됨
+  };
+
+  return (
+    <div className='text-center'>
+      <div className='flex gap-3'>
+        <button onClick={goBack}>Back</button>
+        <button onClick={goHello}>Hello</button>
+        <button onClick={() => changeSearchParams('999')}>change-xxx</button>
+      </div>
+      <span>
+        Me Page: {pathname}?xxx={searchParams.get('xxx')}
+      </span>
+    </div>
+  );
 }
