6ede49a2121cfb82cfc1c1c1188c323b32069806
yermxx <66yrlee99@gmail.com>
2024-12-26 17:11:22 +0900

    ✨ (Nextjs) Data Fetch - SSG, SSR

--- next/app14/app/(routes)/about/[id]/page.tsx
+++ next/app14/app/(routes)/about/[id]/page.tsx
@@ -1,5 +1,15 @@
 import getTodos from '@/lib/todos';
 
+// export const dynamic = 'auto';
+// export const dynamic = 'force-dynamic'; // SSR -> todo를 저장해두지 않고 그때그때 가져옴!!
+
+// export const dynamic = 'force-static'; // SSG
+export async function generateStaticParams() {
+  return (await getTodos(1)).map(({ id }) => ({
+    id: id.toString(),
+  }));
+}
+
 export default async function AboutTodo({
   params: { id },
 }: {
@@ -7,6 +17,7 @@ export default async function AboutTodo({
 }) {
   const todos = await getTodos(1);
   const todo = todos.find((td) => td.id === +id);
+
   // 에러 처리
   if (!todo) {
     return <h1 className='text-2xl text-red-500'>#{id} is not found!!</h1>;
